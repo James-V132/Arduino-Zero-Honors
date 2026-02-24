4882d6aa0bbee0b69b485a85cc9d2cce6d5d5acd
Edwin Kimsal <kimsal.edwin@gmail.com>
2026-02-24 08:29:26 -0500

    Added starter code for API

--- Display_Sensor_Honors/Display_Sensor_Honors.ino
+++ Display_Sensor_Honors/Display_Sensor_Honors.ino
@@ -1,12 +1,25 @@
 #include<LiquidCrystal.h>
 #include<SPI.h>
 #include<MFRC522.h>
+#include <ArduinoHttpClient.h>
+#include <WiFiS3.h> // Or the library for your specific board
+
+// Your network credentials
+const char* ssid = "EK iPhone 16";
+const char* pass = "Password";
 
 LiquidCrystal lcd(9, 10, 5, 4, 3, 2);
 
 #define RST_PIN 8
 #define SS_PIN 7
 MFRC522 myFRID(SS_PIN, RST_PIN);
+
+// Server details
+const char* serverAddress = "IP";
+int port = 80;
+
+WiFiClient client;
+HttpClient http_client = HttpClient(client, serverAddress, port);
 
 void setup() {
   lcd.begin(16,2);
@@ -47,6 +60,8 @@ void loop() {
 
   Serial.println();
   Serial.println();
+  
+  http_client.post("/db/queries/postEntry.sql");
   myFRID.PICC_DumpToSerial(&(myFRID.uid)); // Dumps all info available from card and displays it
 
   lcd.setCursor(0, 0);
